4b5f0b9d5b08715dd9d7b09cf94f2b0f4db63cd0
karan kumar <karanram9876@gmail.com>
2026-09-03 19:28:28 +0530

    Use WebSocket transport

--- client/src/config/socket.js
+++ client/src/config/socket.js
@@ -5,5 +5,5 @@ const SOCKET_URL = import.meta.env.VITE_BASE_URL || "http://localhost:3000";
 export const socket = io(SOCKET_URL, {
     autoConnect: false,
     withCredentials: true,
-    transports: ["polling", "websocket"]
+   transports: ["websocket"]
 })
